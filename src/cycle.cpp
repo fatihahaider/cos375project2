f66baf42680a3ec47aa41c4026dcd0080edebe89
Ryan Salik <rssalik14@gmail.com>
2025-12-11 23:02:39 -0500

    yuh

--- src/cycle.cpp
+++ src/cycle.cpp
@@ -319,7 +319,7 @@ Status runCycles(uint64_t cycles) {
                 pipelineInfo.exInst = prev.idInst;
 
                 // IF -> ID Advancement Logic
-                if (isBranch(prev.ifInst) && prev.ifInst.PC != PC) {
+                if (isBranch(prev.idInst) && prev.ifInst.PC != PC) {
                     // Branch misprediction: squash ID
                     pipelineInfo.idInst = nop(SQUASHED);
                 } else if (iMissCyclesLeft > 0) {
